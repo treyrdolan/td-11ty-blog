309a1fb1ab5d0dcf87fb40ddfcdf20dd52abf80a
treyrdolan <treyrdolan@gmail.com>
2025-12-17 18:02:26 -0500

    Changed dateObject's timezone to UTC for accurate post date and added a sample post

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toLocaleString(DateTime.DATE_MED);
     });
 
   
